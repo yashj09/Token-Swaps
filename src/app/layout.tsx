0cc58756f6c9a9a32b5317647e60d53ff9e5c9bb
Yash Jain <118172073+yashj09@users.noreply.github.com>
2024-11-23 19:39:29 +0530

    provider and tailwind done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
 import "styles/globals.css";
 
 import { Toaster } from "../components/ui/toaster";
-import Provider from "@/lib/provider";
+import { Providers } from "@/lib/provider";
+import { config } from "./config";
+import { headers } from "next/headers";
+import { cookieToInitialState } from "@account-kit/core";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const initialState = cookieToInitialState(
+    config,
+    headers().get("cookie") ?? undefined
+  );
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className="bg-black text-white">
-        <Provider>
-          <main>{children}</main>
-        </Provider>
+        <Providers initialState={initialState}>{children}</Providers>
         <Toaster />
       </body>
     </html>
--- src/components/lib/provider.tsx
+++ src/components/lib/provider.tsx
@@ -1,32 +1,22 @@
 "use client";
-import React from "react";
-import {
-  Web3ReactHooks,
-  Web3ReactProvider,
-  initializeConnector,
-} from "@web3-react/core";
-import { MetaMask } from "@web3-react/metamask";
-const [metaMask, hooks] = initializeConnector<MetaMask>(
-  (actions) =>
-    new MetaMask({
-      actions: {
-        startActivation: actions.startActivation,
-        update: actions.update,
-        resetState: actions.resetState,
-      },
-    })
-);
+import { config, queryClient } from "../../app/config";
+import { AlchemyClientState } from "@account-kit/core";
+import { AlchemyAccountProvider } from "@account-kit/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
 
-const connectors: [MetaMask, Web3ReactHooks][] = [[metaMask, hooks]];
-
-const Provider = ({ children }: { children: React.ReactNode }) => {
+export const Providers = (
+  props: PropsWithChildren<{ initialState?: AlchemyClientState }>
+) => {
   return (
-    <div>
-      <Web3ReactProvider connectors={connectors}>
-        <main>{children}</main>
-      </Web3ReactProvider>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <AlchemyAccountProvider
+        config={config}
+        queryClient={queryClient}
+        initialState={props.initialState}
+      >
+        {props.children}
+      </AlchemyAccountProvider>
+    </QueryClientProvider>
   );
 };
-
-export default Provider;
